a8bd15d047d7f5de456822d521ed6a262232f49d
xMAXSIMKAx <maxymram@gmail.com>
2024-01-04 18:49:51 +0100

    add converter miles to kilometers

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,3 +1,11 @@
 public class Converter {
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
+    }
+
+    public static void printMilesToKilometers(double miles) {
+        double kilometers = milesToKilometers(miles);
+        System.out.println(miles + " миль = " + kilometers + " кілометрів");
+    }
 
 }
