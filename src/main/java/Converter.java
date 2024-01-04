3a6f539eab3ed9f25c144b47c6afa343ab8a3b93
xMAXSIMKAx <maxymram@gmail.com>
2024-01-04 19:09:37 +0100

    Merge develop into the master branch and add info

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,19 +1,28 @@
+import java.util.Scanner;
+
 public class Converter {
+
+    // метод конвертації миль в кілометри
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
     }
 
+    // метод для виведення миль в кілометри
     public static void printMilesToKilometers(double miles) {
         double kilometers = milesToKilometers(miles);
         System.out.println(miles + " миль = " + kilometers + " кілометрів");
     }
+
+    // Метод для конвертації миль в кілометри
     public static double kilometersToMiles(double kilometers) {
         return kilometers / 1.60934;
     }
 
+    // метод для виведення кілометрів в милі
     public static void printKilometersToMiles(double kilometers) {
         double miles = kilometersToMiles(kilometers);
         System.out.println(kilometers + " кілометрів = " + miles + " миль");
     }
 
 }
+
